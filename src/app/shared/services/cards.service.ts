f75431580971959154090265e76d7d1e8884077f
Titov Sergey <sergeyntitov@gmail.com>
2024-06-14 00:45:18 +0300

    removed comments

--- src/app/shared/services/cards.service.ts
+++ src/app/shared/services/cards.service.ts
@@ -1,32 +1,16 @@
 import { Injectable } from '@angular/core';
-import { cards } from "../../data/mock-data";
-import { Card } from "../../types/card";
 import { HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import { map } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardsService {
 
-  // cards: Card[] = cards
-  // apiUrl: string = 'https://api.spaceflightnewsapi.net/v4/articles/'
-
-  // private apiUrl = 'https://spaceflightnewsapi.net/api/v4/articles';
   private baseUrl = 'https://api.spaceflightnewsapi.net/v4';
 
-  // newsApiKey = '2bf3eeaf37954dcd9c94817017169474'
-  // newsApiUrl = 'https://newsapi.org/v2/everything '
-
-  constructor(private http: HttpClient) { }
-
-  // getAll(){
-  //   return this.http.get(this.apiUrl)
-  // }
-
-  // getById(id: string){
-  //   return this.cards.filter(i => i.id === id)
-  // }
+  constructor(private http: HttpClient) {
+  }
 
   getArticles(){
     return this.http.get(`${this.baseUrl}/articles/`).pipe(map((d: any) => d.results));
@@ -35,6 +19,5 @@ export class CardsService {
   getArticleById(id: string){
     return this.http.get(`${this.baseUrl}/articles/${id}`);
   }
-
 }
 
